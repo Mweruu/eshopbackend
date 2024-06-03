6078e287978eed96c800b8150f4e39e7c49daf9b
Mweruu <117080563+Mweruu@users.noreply.github.com>
2024-06-03 16:41:38 +0300

    Update order-item.js

--- database/models/order-item.js
+++ database/models/order-item.js
@@ -1,28 +1,28 @@
-module.exports = (sequelize, DataTypes) => {
-    const OrderItem = sequelize.define('orderItem',{
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-            allowNull: false,
-            unique: true,
-          },
-        quantity:{
-          type:DataTypes.INTEGER,
-          allowNull:false
-        }, 
-        },{});
-        OrderItem.associate = function(models){
-          OrderItem.belongsTo(models.product,{
-            foreignKey:'productId'
-          })
-          OrderItem.belongsTo(models.order,{
-            foreignKey:'orderId'
-          })
-          OrderItem.belongsTo(models.user,{
-            foreignKey:'userId'
-          })
-        };
+// module.exports = (sequelize, DataTypes) => {
+//     const OrderItem = sequelize.define('orderItem',{
+//         id: {
+//             type: DataTypes.INTEGER,
+//             autoIncrement: true,
+//             primaryKey: true,
+//             allowNull: false,
+//             unique: true,
+//           },
+//         quantity:{
+//           type:DataTypes.INTEGER,
+//           allowNull:false
+//         }, 
+//         },{});
+//         OrderItem.associate = function(models){
+//           OrderItem.belongsTo(models.product,{
+//             foreignKey:'productId'
+//           })
+//           OrderItem.belongsTo(models.order,{
+//             foreignKey:'orderId'
+//           })
+//           OrderItem.belongsTo(models.user,{
+//             foreignKey:'userId'
+//           })
+//         };
 
-        return OrderItem;
-};
+//         return OrderItem;
+// };
